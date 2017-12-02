c1bd5aad19f648c9b1e16dfcc006787fc0f1c6d2
Aditya Sripal <adityasripal@gmail.com>
2017-12-01 18:30:32 -0800

    fixes

--- SupplyInsure.sol
+++ SupplyInsure.sol
@@ -1,6 +1,16 @@
 pragma solidity ^0.4.13;
-import "./OracleInterface.sol";
-import "./SupplierOracle.sol";
+
+contract Insurance {
+    function deliver();
+    function refund();
+}
+
+contract Oracle {
+    function subscribe(bytes32 params, address receiver, address sender, uint reward) returns (bool);
+    function confirm() returns (bool);
+    function unsubscribe(bytes32 params) returns (bool);
+    function() payable;
+}
 
 contract SupplyInsure is Insurance {
     address receiver;
@@ -10,7 +20,7 @@ contract SupplyInsure is Insurance {
     uint value;
     uint reward;
 
-    bytes32[] params;
+    bytes32 params;
 
     bool delivered;
     bool delayed;
@@ -21,18 +31,18 @@ contract SupplyInsure is Insurance {
     modifier isSender() {if(msg.sender == sender) {_;}}
     modifier validOracle() {if(msg.sender == oracleAddress && oracleConfirmed) {_;}}
 
-    function SupplyInsure(bytes32[] _params, address _sender, address _oracle, uint _value, uint _reward) payable {
+    function SupplyInsure(bytes32 _params, address _sender, address _oracle, uint _value, uint _reward) payable {
         require(msg.value >= _value + _reward);
         receiver = msg.sender;
         sender = _sender;
         oracleAddress = oracle;
-        oracle = Oracle(_oracle);
+        //oracle = Oracle(_oracle);
         value = _value;
         reward = _reward;
         params = _params;
-        if (!oracle.subscribe(params, receiver, sender, reward)) {
-            revert();
-        }     // returns true on success
+        //if (!oracle.subscribe(params, receiver, sender, reward)) {
+        //    revert();
+        //}     // returns true on success
     }
 
     function confirmTransaction(address _oracle, uint _value) //sender must also confirm address of oracle as well as the value of product
