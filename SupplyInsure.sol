0f5490ec77d54c389653836a0aa90b314009bedd
Aditya Sripal <adityasripal@gmail.com>
2017-12-11 11:24:07 -0800

    Demo ready

--- SupplyInsure.sol
+++ SupplyInsure.sol
@@ -1,42 +1,31 @@
 pragma solidity ^0.4.13;
 
-contract Insurance {
-    function deliver();
-    function refund();
-}
-
-contract Oracle {
+contract SupplierOracle {
     function subscribe(bytes32 params, address receiver, address sender, uint reward) returns (bool);
     function confirm() returns (bool);
     function unsubscribe(bytes32 params) returns (bool);
     function() payable;
 }
 
-contract SupplyInsure is Insurance {
+contract SupplyInsure {
     address receiver;
     address sender;
     address oracleAddress;
-    Oracle oracle;
+    SupplierOracle oracle;
     uint value;
     uint reward;
 
     bytes32 params;
 
-    bool delivered;
-    bool delayed;
-    bool senderConfirmed;
-    bool oracleSubscribed;
-    bool oracleConfirmed;
-
     modifier isSender() {if(msg.sender == sender) {_;}}
-    modifier validOracle() {if(msg.sender == oracleAddress && oracleConfirmed) {_;}}
+    modifier validOracle() {if(msg.sender == oracleAddress) {_;}}
 
     function SupplyInsure(bytes32 _params, address _sender, address _oracle, uint _value, uint _reward) payable {
         require(msg.value >= _value + _reward);
         receiver = msg.sender;
         sender = _sender;
-        oracleAddress = oracle;
-        //oracle = Oracle(_oracle);
+        oracleAddress = _oracle;
+        oracle = SupplierOracle(_oracle);
         value = _value;
         reward = _reward;
         params = _params;
@@ -45,35 +34,17 @@ contract SupplyInsure is Insurance {
         //}     // returns true on success
     }
 
-    function confirmTransaction(address _oracle, uint _value) //sender must also confirm address of oracle as well as the value of product
-    isSender()
-    {
-        if (oracle == _oracle && value == _value) {
-            senderConfirmed = true;
-        } else {
-            revert();
-        }
-        oracleConfirmed = oracle.confirm();
-    }
-
-    function cancelTransaction() {
-        require(msg.sender == receiver && !senderConfirmed);
-        oracle.unsubscribe(params);
-        selfdestruct(receiver);
-    }
-
-    function deliver() 
-    validOracle()
+    function deliver() payable
     {
         sender.transfer(value);
         oracle.transfer(reward);
-        selfdestruct(receiver);
+        receiver.transfer(this.balance);
     }
 
-    function refund()
+    function refund() payable
     validOracle()
     {
         oracle.transfer(reward);
-        selfdestruct(receiver);
+        receiver.transfer(this.balance);
     }
 }
